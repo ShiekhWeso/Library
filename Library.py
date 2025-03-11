c0bd3acc05f0d9f7c1403ef29ccb9fd7900597b3
Weso <mohamed.hosam.eldeeen@gmail.com>
2025-03-11 17:12:14 +0200

    Library.py update 2.0
    Adding the main function so i can access all the other function from it.
    Enhancing the add_book function and other changes to the code.

--- Library.py
+++ Library.py
@@ -9,6 +9,7 @@ def load_library():
     try:
         with open("my_library.json", "r" ) as file:
             library = json.load(file)
+        library = {int(key): value for key,value in library.items()}
     except FileNotFoundError:
         library = {}
     return library   
@@ -19,18 +20,29 @@ def save_library(library):
         
 def add_book(library):
     while True:
-        book_num = int(input("plz enter the book's number: "))
-        book_name = input("plz enter the book's name: ")
-        book_author = input("plz enter the book's author: ")
-        book_status = input("plz enter the book's status: (ava/bor)")
-        choice = input("Do you want to add another book? if you want to exist press (n), if you don't press any botton to continue. ")
+        book_num_input = input("plz enter the book's number: ")
+        if not book_num_input.isdigit():
+            print("Invalid input! The book number must be a positive integer.")
+            continue
+        book_num = int(book_num_input)
+        if book_num in library:
+            print(f"Book number {book_num} is already taken. Please choose a different number.")
+            continue
+        
+        book_name = input("plz enter the book's name: ").strip()
+        book_author = input("plz enter the book's author: ").strip()
+        book_status = input("plz enter the book's status: (ava/bor)").strip().lower()
+        if book_status not in ["ava", "bor"]:
+            print("Invalid status! Enter 'ava' for available or 'bor' for borrowed.")
+            continue
         
         library[book_num] = {"bookname": book_name, "bookauthor": book_author, "bookstatus": book_status}
+        print(f"Book '{book_name}' successfully added to the library!")
         
-        if choice.lower() == "n":
+        choice = input("Do you want to add another book? if you want to exist press (E), if you don't press any botton to continue. ")
+        if choice.lower() == "e":
             break    
-    save_library(My_Library)
-
+    save_library(library)
 
 def display_library(library):
     for key1, value1 in library.items():
@@ -40,22 +52,27 @@ def display_library(library):
         print()
 
 def delete_book(library, n):
-    del library[n]
+    if n in library:
+        n = str(n)
+        del library[n]
+    else:
+        print("This book doesn't exist at the library.")
     save_library(library)
 
 def borrow_book(library, n):
+    n = str(n)
     if n in library:    
         if list(library[n].values())[-1] == "ava":
             library[n]["bookstatus"] = "bor"
             print("You borrowed this book successfully")
         else:
-            print("You can't borrow this booked it's already borrowed.")
+            print("You can't borrow this book it's already borrowed.")
     else:
         print("This book doesn't exist at the library.")
     save_library(library)
     
-    
-def resotre_book(library, n):
+def restore_book(library, n):
+    n = str(n)
     if n in library:    
         if list(library[n].values())[-1] == "bor":
             library[n]["bookstatus"] = "ava"
@@ -66,11 +83,23 @@ def resotre_book(library, n):
         print("This book doesn't exist at the library.")
     save_library(library)
         
+def main():
+    My_Library = load_library()  
+    while True:
+        func = input("1: Display -- 2: Add -- 3:Borrow -- 4: Return -- 5: Delete\nPress (E) to Exit")
+        if func == "1":
+            display_library(My_Library)
+        elif func == "2":
+            add_book(My_Library)
+        elif func == "3":
+            borrow_book(My_Library, int(input("Plz enter the number of book you want to borrow: ")))
+        elif func == "4":
+            restore_book(My_Library, int(input("Plz enter the number of book you want to restore: ")))
+        elif func == "5":
+            delete_book(My_Library, int(input("Plz enter the number of book you want to delete: ")))
+        elif func.lower() == "e":
+            break
+        else:
+            print("Invalid choice!")
         
-My_Library = load_library()
-# delete_book(My_Library, int(input("Plz enter the number of book you want to delete: ")))
-# add_book(My_Library)
-# borrow_book(My_Library, int(input("Plz enter the number of book you want to borrow: ")))
-# resotre_book(My_Library, int(input("Plz enter the number of book you want to restore: ")))
-# save_library(My_Library)
-display_library(My_Library)    
+main()
